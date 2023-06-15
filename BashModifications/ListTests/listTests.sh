0e992dda067486395a09485df014448d10a82d08
NSel1727 <noahseligson18@gmail.com>
2023-06-15 09:54:06 -0400

    Make changes to listTests

--- BashModifications/ListTests/listTests.sh
+++ BashModifications/ListTests/listTests.sh
@@ -20,6 +20,7 @@ usage ()
 testDay=$( date "+%y-%m-%d")
 verbose=0
 rte=0
+
 while [ $# -gt 0 ]
 do 
     param=$1
@@ -46,17 +47,6 @@ done
 if [[ -d ~/smoketest/ScheduleInfos ]]
 then
     find OldPrs/PR-*/ PR-*/ -mtime -1 -iname 'scheduler-*.test' -o -iname 'result*.log' | zip -u ScheduleInfos/Smoketest-logs.zip -@
-
-
-
-
-
-
-
-
-
-
-
 fi
 
 clear
@@ -72,7 +62,7 @@ dayInSec=$(( 3600 * 24 ))
 testDayOffset=$(( ( $(date "+%s") / $dayInSec ) - ( $(date --date "$testDay 12:30" "+%s" ) / $dayInSec ) ))
 [[ $verbose -eq 1 ]] && (echo "testDayOffset: $testDayOffset"; echo "")
 
-res=$( find OldPrs/PR-*/ PR-*/ -daystart -mtime $testDayOffset -iname 'build.summary' -exec egrep -H -i 'skipped' {} \; | sed -e 's/OldPrs\/P\(.*\)$/P\1\/(closed)/'   -e 's/draft\(.*\)/\/(draft)/'  | awk -F '/' '{print $1 $3 }' | sort )
+res=$( find OldPrs/PR-*/ PR-*/ -daystart -mtime $testDayOffset -iname 'build.summary' -exec grep -E -H -i 'skipped' {} \; | sed -e 's/OldPrs\/P\(.*\)$/P\1\/(closed)/'   -e 's/draft\(.*\)/\/(draft)/'  | awk -F '/' '{print $1 $3 }' | sort )
 
 if [[ -n "$res" ]]
 then
@@ -87,9 +77,7 @@ echo "======================="
 
 declare -a PRs
 
-
 cnt=0
-#find OldPrs/PR-*/ PR-*/ -iname 'scheduler-'"$testDay"'*.test' -print | while read fn
 
 while read fn
 do  
@@ -97,17 +85,17 @@ do
 
     #set -x
     version=1
-    item=$(cat $fn | egrep -i 'Instance name|Commit Id|Instance Id' | cut -d' ' -f5 | tr -d \' | paste -d, -s - | cut -d',' -f1,2,3 --output ',' )
+    item=$(cat $fn | grep -E -i 'Instance name|Commit Id|Instance Id' | cut -d' ' -f5 | tr -d \' | paste -d, -s - | cut -d',' -f1,2,3 --output ',' )
 
-    [[ -z "$item" ]] && ( item=$( cat $fn | egrep -i 'Instancename: |CommitId: |:Instance Id|An error' | cut -d' ' -f4,6 | tr -d \' | sed 's/commitId=//' | paste -d, -s - | cut -d',' -f1,2,3 --output ', ' ); version=2 )
-    [[ -z "$item" ]] && ( item=$( cat $fn | egrep -i 'Schedule |sha ' | cut -d ' ' -f1,2,3,4,5 | tr -d \' | tr -d ':' | tr -s ' \t' | paste -d, -s -  ); version=2 )
+    [[ -z "$item" ]] && ( item=$( cat $fn | grep -E -i 'Instancename: |CommitId: |:Instance Id|An error' | cut -d' ' -f4,6 | tr -d \' | sed 's/commitId=//' | paste -d, -s - | cut -d',' -f1,2,3 --output ', ' ); version=2 )
+    [[ -z "$item" ]] && ( item=$( cat $fn | grep -E -i 'Schedule |sha ' | cut -d ' ' -f1,2,3,4,5 | tr -d \' | tr -d ':' | tr -s ' \t' | paste -d, -s -  ); version=2 )
     
-    #timestamp=$( cat $fn | egrep 'Start:' | tr -d '\t' )
+
     timestamp=$( head -n 1 $fn |  cut -d: -f1,2,3 )
-    #echo "$cnt, $item ($timestamp)"
+
     
     IFS=',' read -ra arr <<< "$item"
-    #echo "${arr[@]}"
+
     unset -v IFS # restore IFS to default
 
     if [[ $version -eq 1 ]]
@@ -127,14 +115,14 @@ do
 
     fi
 
-    [[ "$fn" =~ "OldPrs" ]] && printf ", (Closed)" || ( running=$(  egrep -i -c 'Terminate:' $fn ); [[ $running -eq 0 ]] && printf ", (Running)" || printf ", (Finished)")
+    [[ "$fn" =~ "OldPrs" ]] && printf ", (Closed)" || ( running=$(  grep -E -i -c 'Terminate:' $fn ); [[ $running -eq 0 ]] && printf ", (Running)" || printf ", (Finished)")
 
     [[ $verbose -eq 1 ]] && printf ", %s, ( %s )" "$timestamp" "$version"
     printf "\n"
     PRs+=( a[@] )
 
     set +x
-#done < <(find OldPrs/PR-*/ PR-*/ -iname 'scheduler-'"$testDay"'*.test' -print | sort )
+
 # Instance file name structure:
 #               OldPrs/PR-15198/instance-PR-15198-F4CE2C4F-21-07-28_13-59-26.info
 #                       |                 |                 |        |           |             |    |       |      |       |
@@ -151,9 +139,6 @@ do
 #                                                                                                                                                                                           Field 1     Field 4       Field 8           Field 9
 done < <(find OldPrs/PR-*/ PR-*/ -iname 'instance-*-*-'"$testDay"'_*.info' -print | sort  --field-separator='-' --key=1,1 --key=4,4 --key=8.4n,8 --key=9n,9 )
 
-#[[ $cnt -eq 0 ]] && echo "None"
-#[[ -n "$res" ]] && echo "$res" || echo "None"
-
 echo "-----------------------------------------"
 echo ""
 echo "List of results:"; 
@@ -162,17 +147,15 @@ echo ""
 echo "From closed PRs:"
 echo "................"
 
-#res=$( find OldPrs/PR-*/ -iname 'result-'"$testDay"'*.log' -type f -printf "\n" -print -exec bash -c "egrep '\s+Process PR-|\s+sha\s+:|\s+Summary\s+:|\s+pass :' '{}' | tr -d '\t' | tr -s ' ' | paste -d, -s - | sed 's/ : /: /g' | sed -e 's/ : /: /' -e 's/,\(\s*\)/, /g' " \; )
-#[[ -n "$res" ]] && echo "$res" || echo "None"
 cnt=0
 fns=( $( find OldPrs/PR-*/ -iname 'result-'"$testDay"'*.log' -type f  -print  | sort ) )
 for fn in ${fns[@]}
 do
     cnt=$(( $cnt + 1 ))
-    #                                                                                                            remove tabs, multi spaces   put all in one    remove leading spaces                  remove second PR num and label            truncate commit id to 8 chars and reassemble the  record
-    item=$(egrep '\s+Process PR-|\s+sha\s+:|\s+Summary\s+:|\s+pass :|In PR-' $fn | tr -d '\t' | tr -s ' ' | paste -d, -s - | sed -e 's/ : /: /' -e 's/,\(\s*\)/, /g' -e 's/In PR-[0-9]* , label: [a-zA-Z0-9]* : //g' -e 's/^[0-9]*\/[0-9]*\.\s//g' | sed -r 's/(.*) ((sha: \w{8,8})([a-zA-Z0-9]+)), (.*)/\1 \3, \5/' )
+                                                                                                         #remove tabs, multi spaces, put all in one, remove leading spaces, remove second PR num and label truncate commit id to 8 chars and reassemble the  record
+    item=$(grep -E '\s+Process PR-|\s+sha\s+:|\s+Summary\s+:|\s+pass :|In PR-' $fn | tr -d '\t' | tr -s ' ' | paste -d, -s - | sed -e 's/ : /: /' -e 's/,\(\s*\)/, /g' -e 's/In PR-[0-9]* , label: [a-zA-Z0-9]* : //g' -e 's/^[0-9]*\/[0-9]*\.\s//g' | sed -r 's/(.*) ((sha: \w{8,8})([a-zA-Z0-9]+)), (.*)/\1 \3, \5/' )
 
-    timestamp=$( egrep 'Finished:' $fn | tr -d '\t' | cut -d ' ' -f 1,2,3 | tr -d ',' )
+    timestamp=$( grep -E 'Finished:' $fn | tr -d '\t' | cut -d ' ' -f 1,2,3 | tr -d ',' )
     [[ -z "$item" ]] && echo -e "$fn\n\tNONE" ||  printf "%s, %s" "$cnt" "$item";  [[ $verbose -eq 1 ]] && printf " ( %s) " "$timestamp";  printf "\n"
 done
 [[ ${#fns[@]} -eq 0 ]] && echo "None"
@@ -181,17 +164,16 @@ echo ""
 
 echo "From open PRs:"
 echo ".............."
-#res=$( find PR-*/ -iname 'result-'"$testDay"'*.log' -type f -printf "\n" -print -exec bash -c "egrep '\s+Process PR-|\s+sha\s+:|\s+Summary\s+:|\s+pass :' '{}' | tr -d '\t' | tr -s ' ' | paste -d, -s - | sed 's/ : /: /g' | sed -e 's/ : /: /' -e 's/,\(\s*\)/, /g' " \; )
-#[[ -n "$res" ]] && echo "$res" || echo "None"
+
 cnt=0
 fns=( $( find PR-*/ -iname 'result-'"$testDay"'*.log' -type f  -print | sort ) )
 for fn in ${fns[@]}
 do
     cnt=$(( $cnt + 1 ))
     
-    item=$(egrep '\s+Process PR-|\s+sha\s+:|\s+Summary\s+:|\s+pass :|In PR-' $fn | tr -d '\t' | tr -s ' ' | paste -d, -s - | sed -e 's/ : /: /' -e 's/,\(\s*\)/, /g' -e 's/In PR-[0-9]* , label: [a-zA-Z0-9]* : //g' -e 's/^[0-9]*\/[0-9]*\.\s//g' | sed -r 's/(.*) ((sha: \w{8,8})([a-zA-Z0-9]+)), (.*)/\1 \3, \5/' )
+    item=$(grep -E '\s+Process PR-|\s+sha\s+:|\s+Summary\s+:|\s+pass :|In PR-' $fn | tr -d '\t' | tr -s ' ' | paste -d, -s - | sed -e 's/ : /: /' -e 's/,\(\s*\)/, /g' -e 's/In PR-[0-9]* , label: [a-zA-Z0-9]* : //g' -e 's/^[0-9]*\/[0-9]*\.\s//g' | sed -r 's/(.*) ((sha: \w{8,8})([a-zA-Z0-9]+)), (.*)/\1 \3, \5/' )
 
-    timestamp=$( egrep 'Finished:' $fn | tr -d '\t' | cut -d ' ' -f 1,2,3 | tr -d ',' | tr -d '\n' )
+    timestamp=$( grep -E 'Finished:' $fn | tr -d '\t' | cut -d ' ' -f 1,2,3 | tr -d ',' | tr -d '\n' )
     [[ -z "$item" ]] && echo -e "$fn\n\tNONE" ||  printf "%s, %s" "$cnt" "$item";  [[ $verbose -eq 1 ]] && printf " ( %s) " "$timestamp";  printf "\n"
 done
 
@@ -207,29 +189,16 @@ then
     echo ""
     echo "From closed PRs:"
     echo "................"
-    #find OldPrs/PR-* -iname 'RelWithDebInfo_Build*' -type f -exec egrep -H 'RTE changed' '{}' \;
-    find OldPrs/PR-* -iname 'prp-*' -type f -exec egrep -H ' testing/regress/(ecl-|hpcc/)' '{}' \;
+
+    find OldPrs/PR-* -iname 'prp-*' -type f -exec grep -E -H ' testing/regress/(ecl-|hpcc/)' '{}' \;
 
     echo ""
     echo "From open PRs:"
     echo "................"
-    #find PR-*/ -iname 'RelWithDebInfo_Build*' -type f -exec egrep -H 'RTE changed' '{}' \;
-    find PR-*/ -iname 'prp-*' -type f -exec egrep -H ' testing/regress/(ecl-|hpcc/)' '{}' \;
+
+    find PR-*/ -iname 'prp-*' -type f -exec grep -E -H ' testing/regress/(ecl-|hpcc/)' '{}' \;
     echo "................"
 fi
 echo ""
 echo "End."
-
-# To create one line digest with all information what we need
-#unset -v IFS # restore IFS to default
-#
-#echo "PRs:"
-#max=${#PRs[@]}
-#for ((i=0; i <$max; i++))
-#do
-#    pr=${!PRs[$i]}
-#    echo "$i -> ${pr}"
-#done
-
-
 
